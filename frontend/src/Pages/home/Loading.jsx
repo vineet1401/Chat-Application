2165554b73f45ed03e98efcc4223701901f0e232
vineet1401 <sakpalvineet1401@gmail.com>
2024-04-13 17:15:43 +0530

    Socket implemented

--- frontend/src/Pages/home/Loading.jsx
+++ frontend/src/Pages/home/Loading.jsx
@@ -1,24 +1,17 @@
-import Lottie from 'react-lottie';
-import Messenger from "../../assets/lottie/Messenger.json";
+import { Triangle } from "react-loader-spinner";
 
 export default function Loading() {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: Messenger,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice"
-        }
-      };
-    
-    return (
-      <div className='flex h-[100vh] justify-center items-center'>
-        <Lottie 
-          options={defaultOptions}
-          height={"full"}
-          width={"full"}
-        />
-      </div>
-    );
-  }
-  
+  return (
+    <div className="h-[100vh] flex justify-center items-center">
+      <Triangle
+        visible={true}
+        height="150"
+        width="150"
+        color="#0000ff"
+        ariaLabel="triangle-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
+}
